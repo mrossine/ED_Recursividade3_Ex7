1c11567adb4bd4b22eaa2cbd4105449e509ba755
Matheus Rossine <matheusrrodrigues2001@gmail.com>
2025-02-19 01:22:40 -0300

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,9 +5,18 @@ public class Controller {
 		super();
 	}
 	public int somaAlgarismos(int [] vetor, int tamanho) {
+		/*
+		 * Faz a parada quando chega na primeira posição do vetor que
+		 * contém os algarimos do número da entrada de dados
+		 */
 		if(tamanho == 0) {
 			return vetor[tamanho];
 		}
+		/*
+		 * Realiza a soma dos valores presentes nas posições do vetor
+		 * e entra na recursiva para continuar fazendo a adição, até o 
+		 * ponto de parada
+		 */
 		else {
 			return vetor[tamanho] + somaAlgarismos(vetor, tamanho - 1);
 		}
